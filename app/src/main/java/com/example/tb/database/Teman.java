d28f311fd81e010aa397bf7a1cea7ba43930fb62
bintangaji <bintangaji@gmail.com>
2021-06-26 09:26:51 +0700

    TestBarang

---
+++ app/src/main/java/com/example/tb/database/Teman.java
@@ -0,0 +1,47 @@
+package com.example.barangtes.database;
+
+import java.io.Serializable;
+
+public class Teman implements Serializable {
+    String nama;
+    String telpon, kode;
+
+    public Teman() {
+    }
+
+    public Teman(String nama, String telpon) {
+        this.nama = nama;
+        this.telpon = telpon;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getKode(){
+        return kode;
+    }
+    public void setKode(String kode){
+        this.kode = kode;
+    }
+
+    public String getTelpon() {
+        return telpon;
+    }
+
+    public void setTelpon(String telpon) {
+        this.telpon = telpon;
+    }
+
+    @Override
+    public String toString() {
+        return "Teman{" +
+                "nama='" + nama + '\'' +
+                ", telpon='" + telpon + '\'' +
+                '}';
+    }
+}
